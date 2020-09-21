28828297302e10f868a72a9160384cdb873c869a
Elijah Curtis <ejcurtis001@gmail.com>
2020-09-21 20:38:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,3 +3,9 @@ def take_a_number(line,joe)
   line << joe
   puts "welcome, #{joe}.You are number #{line.length} in line."
 end 
+def line (katz_deli)
+  if katz_deli.length==0 
+    puts "line is empty"
+  end
+end
+
